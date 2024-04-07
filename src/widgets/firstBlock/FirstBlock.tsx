dae1ffa0d70b5cf8a77f1024c56f9ade23028bfd
warnoffline <hametshin543@gmail.com>
2024-04-07 17:30:23 +0300

    animation + change footer

--- src/widgets/firstBlock/FirstBlock.tsx
+++ src/widgets/firstBlock/FirstBlock.tsx
@@ -1,18 +1,25 @@
 import './FirstBlock.scss'
 import Button from '../../shared/button/Button';
+import {motion} from 'framer-motion'
+const animation = {
+    initial: { opacity: 0, y: 50 },
+    whileInView: { opacity: 1, y: 0 },
+    viewport: { once: true },
+    transition: { duration: 1.5 }
+}
 const FirstBlock = () => {
     return (
         <div className='first'>
             <div className='first__center'>
                 <img src="./blur-stars.png" alt="#" className='first__image first__image--left' />
                 <img src="./star.png" alt="#" className='first__image first__image--right' />
-                <h1>Web Studio helping startup's</h1>
-                <div className='first__center-down'>
-                    <h2>Команда, понимающая ваши проблемы</h2>
+                <motion.h1 {...animation}>Web Studio helping startup's</motion.h1>
+                <motion.div {...animation }className='first__center-down'>
+                    <h2 {...animation}>Команда, понимающая ваши проблемы</h2>
                     <div>
                         <Button>Оставить заявку</Button>
                     </div>
-                </div>
+                </motion.div>
             </div>
         </div>
     )
--- src/widgets/secondBlock/SecondBlock.tsx
+++ src/widgets/secondBlock/SecondBlock.tsx
@@ -2,16 +2,23 @@ import './SecondBlock.scss'
 import img1 from './img/1.svg'
 import img2 from './img/2.svg'
 import img3 from './img/3.svg'
+import {motion} from 'framer-motion'
+const animation = {
+    initial: { opacity: 0, y: 50 },
+    whileInView: { opacity: 1, y: 0 },
+    viewport: { once: true },
+    transition: { duration: 1.5 }
+}
 const SecondBlock = () => {
     return(
         <div className="second">
             <div className="second__center">
                 <img className='img1' src={img1} alt="" />
-                <h1 className='section__first'>Молодая креативная студия</h1>
-                <p className='section__second'>Мы - участники программы ФСИ, понимаем ваши потребности и помогаем с их закрытием </p>
+                <motion.h1 {...animation} className='section__first'>Молодая креативная студия</motion.h1>
+                <motion.p {...animation} className='section__second'>Мы - участники программы ФСИ, понимаем ваши потребности и помогаем с их закрытием </motion.p>
                 <img className='img2' src={img2} alt="" />
-                <p className='section__third'>Разрабатываем сайты понимая ваши проблемы и решая их доступными способом </p>
-                <h1 className='section__fourth'>Развиваем вас и ваш бренд</h1>
+                <motion.p {...animation} className='section__third'>Разрабатываем сайты понимая ваши проблемы и решая их доступными способом </motion.p>
+                <motion.h1 {...animation} className='section__fourth'>Развиваем вас и ваш бренд</motion.h1>
                 <img className='img3' src={img3} alt="" />
             </div>
         </div>
