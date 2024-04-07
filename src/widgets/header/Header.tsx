afb072798a6ad6b30cf84d499068cede8b00211b
ozzzpo <onehendrix04@gmail.com>
2024-04-07 21:18:01 +0300

    minor changes

--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -16,7 +16,7 @@ const Header = ({isOpen, onOpen, onClose} : HeaderProps) => {
                 <nav className='head__links'>
                     <a href="#">О нас</a>
                     <div className="head__links-line"></div>
-                    <a href="#">Преимущества</a>ы
+                    <a href="#">Преимущества</a>
                     <div className="head__links-line"></div>
                     <a href="#">Услуги</a>
                 </nav>
