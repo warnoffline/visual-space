ad4baf3d77b01006208d21c657be7b5ead7dd4aa
ozzzpo <onehendrix04@gmail.com>
2024-04-08 15:28:00 +0300

    change modal state

--- src/features/send-personal-data/ui/SendPersonalForm.tsx
+++ src/features/send-personal-data/ui/SendPersonalForm.tsx
@@ -1,39 +1,40 @@
-import {Input, Textarea } from "@chakra-ui/react";
+import {Heading, Input, Textarea } from "@chakra-ui/react";
 import Button from "../../../shared/button/Button";
 import { useForm } from "react-hook-form";
 import "./SendPersonalForm.scss"
-type SendPersonalFormProps = {
-    onClose: () => void;
-}
-export function SendPersonalForm({onClose}: SendPersonalFormProps) {
-    const {register, handleSubmit, formState:{errors}} = useForm()
+
+export function SendPersonalForm() {
+    const {register, handleSubmit, formState:{errors, isSubmitted}} = useForm()
     const onSubmit = (data: unknown) => {
-        console.log(data)
-        onClose();
+        console.log(data);
     }
     return <form className="send-personal" onSubmit={handleSubmit(onSubmit)}>
-        <div className="send-personal__inputs">
-            <div className="send-personal__block">
-<               Input type="text" className="send-personal__input send-personal__input--upper" placeholder="Ваше имя" {...register("name", {required: true})} isInvalid={!!errors.name}/>
-                {errors.name && (
-                <p className="send-personal__error">Введите имя</p>
-                )}
-            </div>
+        <Heading fontSize="xxx-large" fontWeight={900} fontFamily="Unbounded" className="send-personal__heading">{isSubmitted ? "Заявка отправлена" : "Оставить заявку"}</Heading>
+        <p className="send-personal__text">{isSubmitted ? "Свяжемся с вами в ближайшее время для предоставления консультации" : "Заполните форму и наши сотрудники свяжутся с вами для консультации"}</p>
+        {isSubmitted ? "" : <>
+            <div className="send-personal__inputs">
+                <div className="send-personal__block">
+<                   Input type="text" className="send-personal__input send-personal__input--upper" placeholder="Ваше имя" {...register("name", {required: true})} isInvalid={!!errors.name}/>
+                    {errors.name && (
+                    <p className="send-personal__error">Введите имя</p>
+                    )}
+                </div>
             
-            <div className="send-personal__block">
-                <Input type="phone" className="send-personal__input send-personal__input--lower" placeholder="Номер телефона"  {...register("phone", {required: true, pattern: /^\+?\d{11}$/})} isInvalid={!!errors.phone}/>
-                {errors.phone && (
-                <p className="send-personal__error">Введите корректный телефон</p>
-                )}
+                <div className="send-personal__block">
+                    <Input type="phone" className="send-personal__input send-personal__input--lower" placeholder="Номер телефона"  {...register("phone", {required: true, pattern: /^\+?\d{11}$/})} isInvalid={!!errors.phone}/>
+                    {errors.phone && (
+                    <p className="send-personal__error">Введите корректный телефон</p>
+                    )}
+                </div>
+
+                <Textarea resize="none" className="send-personal__textarea" placeholder="Комментарий к проекту"  {...register("comment")} />
             </div>
 
-           <Textarea resize="none" className="send-personal__textarea" placeholder="Комментарий к проекту"  {...register("comment")} />
-
-        </div>
-
-        <div className="send-personal__submit">
-           <Button fz={20}>Отправить заявку</Button>
-           <p className="send-personal__agreement">Я даю согласие, что студия Visual Space собирает и хранит личные данные, указанные в этой контактной форме.</p>
-        </div>
+            <div className="send-personal__submit">
+                <Button fz={20}>Отправить заявку</Button>
+                <p className="send-personal__agreement">Я даю согласие, что студия Visual Space собирает и хранит личные данные, указанные в этой контактной форме.</p>
+            </div>
+            </>}
     </form>
 }
+
--- src/widgets/MyModal/ui/MyModal.tsx
+++ src/widgets/MyModal/ui/MyModal.tsx
@@ -1,5 +1,5 @@
 
-import { Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay } from "@chakra-ui/react";
+import { Modal, ModalBody, ModalCloseButton, ModalContent,  ModalOverlay } from "@chakra-ui/react";
 import { SendPersonalForm } from "../../../features/send-personal-data";
 import "./MyModal.scss"
 type ModalProps = {
@@ -10,11 +10,9 @@ export function MyModal({isOpen, onClose}: ModalProps) {
     return <Modal isOpen={isOpen} onClose={onClose} isCentered closeOnEsc>
     <ModalOverlay />  
     <ModalContent className="modal" bg="#0F0F0F" borderRadius={50} py={5} px={2}>
-      <ModalHeader className="modal__header" fontSize={40} fontWeight={900}>Оставить заявку</ModalHeader>
       <ModalCloseButton w={50} h={50} color="#e6e6e6" size="xl"/>
       <ModalBody display="flex" flexDir="column" gap={5}>
-      <p className="modal__text">Заполните форму и наши сотрудники свяжутся с вами для консультации</p>
-      <SendPersonalForm onClose={onClose}/>
+      <SendPersonalForm/>
       </ModalBody>
 
     </ModalContent>
